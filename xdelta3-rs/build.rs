b5404e1fde3a52ef904be6d6319875ee0998b03f
axo <>
2025-12-18 22:14:52 +0000

    Included xdelta3-rs with the project
    FossilOrigin-Name: 99b58c11021104a7030b31180d6c60a8ee8ae7b11b2e8d8e2d136c2b020aecc8

---
+++ xdelta3-rs/build.rs
@@ -0,0 +1,118 @@
+extern crate bindgen;
+extern crate cc;
+
+use std::env;
+use std::process::Command;
+
+use rand::Rng;
+use std::fs::{remove_file, File};
+use std::io::Write;
+use std::path::PathBuf;
+
+fn add_def(v: &mut Vec<(String, String)>, key: &str, val: &str) {
+    v.push((key.to_owned(), val.to_owned()));
+}
+
+fn main() {
+    let mut defines = Vec::new();
+    for i in &[
+        "size_t",
+        "unsigned int",
+        "unsigned long",
+        "unsigned long long",
+    ] {
+        let def_name = format!("SIZEOF_{}", i.to_uppercase().replace(" ", "_"));
+        defines.push((def_name, check_native_size(i)));
+    }
+    add_def(&mut defines, "SECONDARY_DJW", "1");
+    add_def(&mut defines, "SECONDARY_FGK", "1");
+    add_def(&mut defines, "EXTERNAL_COMPRESSION", "0");
+    add_def(&mut defines, "XD3_USE_LARGEFILE64", "1");
+
+    #[cfg(windows)]
+    add_def(&mut defines, "XD3_WIN32", "1");
+    add_def(&mut defines, "SHELL_TESTS", "0");
+
+    #[cfg(feature = "lzma")]
+    {
+        add_def(&mut defines, "SECONDARY_LZMA", "1");
+        pkg_config::Config::new().probe("liblzma").unwrap();
+    }
+
+    {
+        let mut builder = cc::Build::new();
+        builder.include("xdelta3");
+        for (key, val) in &defines {
+            builder.define(&key, Some(val.as_str()));
+        }
+
+        builder
+            .file("xdelta3/xdelta3.c")
+            .warnings(false)
+            .compile("xdelta3");
+    }
+
+    {
+        let mut builder = bindgen::Builder::default();
+
+        for (key, val) in &defines {
+            builder = builder.clang_arg(format!("-D{}={}", key, val));
+        }
+        let bindings = builder
+            .header("xdelta3/xdelta3.h")
+            .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+            .allowlist_function("xd3_.*")
+            .allowlist_type("xd3_.*")
+            .rustified_enum("xd3_.*")
+            .generate()
+            .expect("Unable to generate bindings");
+
+        // Write the bindings to the $OUT_DIR/bindings.rs file.
+        let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+        bindings
+            .write_to_file(out_path.join("bindings.rs"))
+            .expect("Couldn't write bindings!");
+    }
+}
+
+fn check_native_size(name: &str) -> String {
+    let builder = cc::Build::new();
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let compiler = builder.get_compiler();
+    let mut compile = Command::new(compiler.path().as_os_str());
+    let test_code = format!("#include <stdint.h>\n#include <stdio.h>\nint main() {{printf(\"%lu\", sizeof({})); return 0;}}\n", name);
+    // didn't use tempfile since tempfile was having issues on Windows
+    let mut rng = rand::thread_rng();
+    let test_binary_fn = format!("{}/test-{}", out_dir, rng.gen::<i32>());
+
+    #[cfg(windows)]
+    let test_binary_fn = format!("{}.exe", test_binary_fn);
+
+    let test_source_fn = format!("{}/test-{:x}.c", out_dir, rng.gen::<i32>());
+    let mut test_source = File::create(&test_source_fn).expect("Error creating test compile files");
+
+    compile.args(compiler.args()).current_dir(out_dir);
+    if compiler.is_like_msvc() {
+        compile.args(&[&test_source_fn, &format!("/Fe{}", test_binary_fn)]);
+    } else {
+        compile.args(&[&test_source_fn, "-o", &test_binary_fn]);
+    }
+    test_source
+        .write_all(test_code.as_bytes())
+        .expect("Error writing test compile files");
+    drop(test_source); // close the source file, otherwise there will be problems on Windows
+    for &(ref a, ref b) in compiler.env().iter() {
+        compile.env(a, b);
+    }
+    compile.output().expect("Error compiling test source");
+    remove_file(test_source_fn).ok();
+
+    compile = Command::new(&test_binary_fn);
+    let output = compile
+        .output()
+        .expect("Error executing test binary")
+        .stdout;
+    let output = String::from_utf8(output).expect("Error converting Unicode sequence");
+    remove_file(test_binary_fn).ok();
+    return output;
+}
---
+++ xdelta3-rs/src/lib.rs
@@ -0,0 +1,127 @@
+//! # xdelta3
+//!
+//! This crate is a Rust binding of `xdelta3` which is written in C.
+//!
+//! In case you are not familar, `xdelta3` implements VCDIFF standard, which is a standard for
+//! binary patches.
+//! You can generate and apply VCDIFF patch for two similar (but large) binary files quickly using `xdelta3`.
+//!
+//! The original `xdelta3` utility is widely used for delivering software updates and ROM hacks.
+//!
+//! You can find out how to use this crate in this documentation and you can also consult the tests in the `test/` folder
+//! to see it in action (how to generate and patch two files!)
+
+extern crate libc;
+
+use libc::c_uint;
+
+#[allow(dead_code)]
+mod binding {
+    #![allow(non_upper_case_globals)]
+    #![allow(non_camel_case_types)]
+    #![allow(non_snake_case)]
+
+    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+}
+
+/// Function to generate the difference data
+///
+/// This function is used to generate the difference data.
+/// The data in `src` will be treated as "original" data and the
+/// data in `input` will be treated as "after", "patched" or "expected" data.
+///
+/// If you want to build an application that applies patches or differential updates,
+/// this function is used to generate the patch data (or update files).
+/// When generating the patch file, you might want to read your old file into a `&[u8]` and
+/// pass that variable to the `src` parameter and read your new file into another `&[u8]` and
+/// pass that variable to the `input` parameter. And then you could write the output of this function
+/// to a file.
+///
+/// Here is a basic example to show how to use this function:
+/// ```
+/// extern crate xdelta3;
+/// use xdelta3::encode;
+///
+/// fn main() {
+///     let result = encode(&[1, 2, 3, 4, 5, 6, 7], &[1, 2, 4, 4, 7, 6, 7]);
+///     assert_eq!(result.unwrap().as_slice(), &[214, 195, 196, 0, 0, 0, 13, 7, 0, 7, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8]);
+/// }
+/// ```
+///
+/// You might notice the generated patch data is larger than both orginal data and the updated data.
+/// But don't worry, if your data is large enough and kind of similar between each other (usually the case
+/// for software updates or ROM patches), the patch data should be only a fraction of your updated file.
+pub fn encode(input: &[u8], src: &[u8]) -> Option<Vec<u8>> {
+    unsafe {
+        let input_len = input.len() as c_uint;
+        let src_len = src.len() as c_uint;
+        let estimated_out_len = (input_len + src_len) * 2;
+        let mut avail_output = 0 as c_uint;
+        let mut output = Vec::with_capacity(estimated_out_len as usize);
+        let result = binding::xd3_encode_memory(
+            input.as_ptr(),
+            input_len,
+            src.as_ptr(),
+            src_len,
+            output.as_mut_ptr(),
+            &mut avail_output,
+            estimated_out_len,
+            0,
+        );
+        if result == 0 {
+            output.set_len(avail_output as usize);
+            Some(output)
+        } else {
+            None
+        }
+    }
+}
+
+/// Function to decode the difference data
+///
+/// This function is used to decode the difference data.
+/// The data in `src` will be treated as "original" data and the
+/// data in `input` will be treated as "difference" or "patch" data.
+/// The returned `Vec` stores the data that has been patched
+///
+/// As opposed to the encode function, if you are building an application that
+/// applies patches or differential updates, this function should be used to
+/// patch or update the old file from the patch data. It's recommeded to check
+/// for the file integrity after doing the decode to prevent from creating
+/// potentially corrupted files
+///
+/// Here is a basic example to show how to use this function:
+/// ```
+/// extern crate xdelta3;
+/// use xdelta3::decode;
+///
+/// fn main() {
+///     let result = decode(&[214, 195, 196, 0, 0, 0, 13, 7, 0, 7, 1, 0, 1, 2, 3, 4, 5, 6, 7, 8], &[1, 2, 4, 4, 7, 6, 7]);
+///     assert_eq!(result.unwrap().as_slice(), &[1, 2, 3, 4, 5, 6, 7]);
+/// }
+/// ```
+pub fn decode(input: &[u8], src: &[u8]) -> Option<Vec<u8>> {
+    unsafe {
+        let input_len = input.len() as c_uint;
+        let src_len = src.len() as c_uint;
+        let estimated_out_len = (input_len + src_len) * 2;
+        let mut avail_output = 0 as c_uint;
+        let mut output = Vec::with_capacity(estimated_out_len as usize);
+        let result = binding::xd3_decode_memory(
+            input.as_ptr(),
+            input_len,
+            src.as_ptr(),
+            src_len,
+            output.as_mut_ptr(),
+            &mut avail_output,
+            estimated_out_len,
+            0,
+        );
+        if result == 0 {
+            output.set_len(avail_output as usize);
+            Some(output)
+        } else {
+            None
+        }
+    }
+}
